cf00b3f72e3c0aa3ec55b288ff9b2eb0f4d9645b
Viktor K <popvik13@mail.ru>
2023-02-19 19:03:11 +0300

    Sozdal papku Seminar004 i dobavil 4 Examples

---
+++ Seminar004/Example004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+void FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0,2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    // for (int i = 0; i < arr.Length; i++)
+    // {
+    // System.Console.Write(arr[i] + " ");
+    // }    
+    Console.WriteLine("[" + string.Join(", ", FillArray) + "]");
+}
