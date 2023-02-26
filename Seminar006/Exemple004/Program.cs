f6aada76820a7afab2edb52795c62b2c174679ef
Viktor K <popvik13@mail.ru>
2023-02-26 22:25:29 +0300

    V Seminar006 Sozdal papku Example004 i dobavil v nee programmu

---
+++ Seminar006/Exemple004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 44 -> 101100
+// 3  -> 11
+// 2  -> 10
+// Второй вариант решения задачи через Рекурсивную функцию, тоесть функция в функции вызывающая сама себя
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetBinaryView(int num)
+{
+    if (num == 0) return;
+    GetBinaryView(num / 2);
+    System.Console.Write(num % 2);
+}
+
+int N = ReadInt("Введите число");
+GetBinaryView(N);
+System.Console.WriteLine();
+System.Console.WriteLine(Convert.ToString(N,2)); // Переводит значение в строкову переменную двоичной системы
